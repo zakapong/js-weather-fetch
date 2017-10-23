bdeaed85f8f115d3c53c004fc4f93d8eaefa65cd
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-10-23 10:43:47 +0200

    first commit with all files

---
+++ app.js
@@ -0,0 +1,19 @@
+var temp;
+var loc;
+var icon;
+var humidity;
+var wind;
+var direction;
+
+
+window.onload = function () {
+    temp = document.getElementById("temperature");
+    loc = document.getElementById("location");
+    icon = document.getElementById("icon");
+    humidity = document.getElementById("humidity");
+    wind = document.getElementById("wind");
+    direction = document.getElementById("direction");
+
+
+
+}
