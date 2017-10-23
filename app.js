f50fa2a8c19a8c4e6a1a61787e5b236a3323ca32
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-10-23 11:01:22 +0200

    manually adding data

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ var humidity;
 var wind;
 var direction;
 
+function update(weather) {
+    icon.src = "imgs/codes/" + weather.icon + ".png"
+    humidity.innerHTML = weather.humidity;
+    wind.innerHtml = weather.wind;
+    direction.innerHTML = weather.direction;
+    loc.innerHTML = weather.loc;
+    temp.innerHTML = weather.temp;
+}
 
 window.onload = function () {
     temp = document.getElementById("temperature");
@@ -14,6 +22,14 @@ window.onload = function () {
     wind = document.getElementById("wind");
     direction = document.getElementById("direction");
 
-
+  var weather = {};
+    weather.wind = 3.5;
+    weather.direction = "N";
+    weather.humidity = 35;
+    weather.loc = "Boston";
+    weather.temp = "45";
+    weather.icon = 200;
+    
+    update(weather);
 
 }
