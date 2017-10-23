868f17d8bfe9869dd1896fe452632aadb61215f9
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-10-23 15:30:02 +0200

    adding all functionality to show weather dinamically

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var APPID = "8416bb967d93c8de6bd049a9b4180c27";
 var temp;
 var loc;
 var icon;
@@ -5,13 +6,91 @@ var humidity;
 var wind;
 var direction;
 
+
+function updateByZip(zip){
+     var url = "http://api.openweathermap.org/data/2.5/weather?" +
+	"zip=" + zip +
+	"&APPID=" + APPID;
+    sendRequest(url);  
+    
+   
+}
+
+function updateByGeo(lat, lon){
+    var url = "http://api.openweathermap.org/data/2.5/weather?" +
+	"lat=" + lat +
+	"&lon=" + lon +
+	"&APPID=" + APPID;
+    sendRequest(url);    
+}
+
+
+function sendRequest(url){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var data = JSON.parse(xmlhttp.responseText);
+	    var weather = {};
+	    weather.icon = data.weather[0].id;
+	    weather.humidity = data.main.humidity;
+	    weather.wind = data.wind.speed;
+
+	    weather.direction = degreesToDirection(data.wind.deg);
+	    weather.loc = data.name;
+	 
+	    weather.temp = K2C(data.main.temp);
+	    update(weather);
+    } 
+        
+    };
+
+    xmlhttp.open("GET", url, true);
+    
+    xmlhttp.onload = function () {
+    if (xmlhttp.status == 404){
+        console.log("kopa kopa")
+    }
+};
+
+
+    xmlhttp.send();
+   
+}
+
+function degreesToDirection(degrees){
+    var range = 360/16;
+    var low = 360 - range/2;
+    var high = (low + range) % 360;
+    var angles = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    for( i in angles ) {
+	if(degrees >= low && degrees < high){
+	    console.log(angles[i]);
+	    return angles[i];
+	    console.log("derp");
+	}
+	low = (low + range) % 360;
+	high = (high + range) % 360;
+    }
+    return "N";
+    
+}
+
+
+function K2F(k){
+    return Math.round(k*(9/5)-459.67);
+}
+
+function K2C(k){
+    return Math.round(k - 273.15);
+}
+
 function update(weather) {
-    icon.src = "imgs/codes/" + weather.icon + ".png"
-    humidity.innerHTML = weather.humidity;
-    wind.innerHtml = weather.wind;
+    wind.innerHTML =  weather.wind;
     direction.innerHTML = weather.direction;
+    humidity.innerHTML = weather.humidity;
     loc.innerHTML = weather.loc;
     temp.innerHTML = weather.temp;
+    icon.src = "imgs/codes/" + weather.icon + ".png";
 }
 
 window.onload = function () {
@@ -22,14 +101,14 @@ window.onload = function () {
     wind = document.getElementById("wind");
     direction = document.getElementById("direction");
 
-  var weather = {};
-    weather.wind = 3.5;
-    weather.direction = "N";
-    weather.humidity = 35;
-    weather.loc = "Boston";
-    weather.temp = "45";
-    weather.icon = 200;
-    
-    update(weather);
+    if(navigator.geolocation){
+	var showPosition = function(position){
+	    updateByGeo(position.coords.latitude, position.coords.longitude);
+	}
+	navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+	var zip = window.prompt("Could not discover your location. What is your zip code?");
+	updateByZip(zip);
+    }
 
 }
